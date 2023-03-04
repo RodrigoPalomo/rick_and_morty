b6008bc06a72ab87ff7cbb1a81221e2341c830fc
Rodrigo Palomo <repmlm@hotmail.com>
2023-03-04 17:31:32 -0300

    corrections

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import "./App.css";
-// import Card from './components/Card/Card.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// COMPONENTES
 import Cards from "./components/Cards/Cards.jsx";
 import Nav from "./components/Nav/Nav";
-// import SearchBar from './components/SearchBar/SearchBar'
-// import characters, { Rick } from './data.js'
+// COMPONENTES
 
 function App() {
   const [characters, setCharacters] = useState([]);
+
+  let onClose = (id) => {
+    // const newCharacters = characters.filter((character) => character.id !== id);
+    // setCharacters(newCharacters);
+    setCharacters(characters.filter((character) => character.id !== id));
+  };
+
   function onSearch(character) {
     fetch(`https://rickandmortyapi.com/api/character/${character}`)
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((data) => {
         if (data.name) {
           setCharacters((oldChars) => [...oldChars, data]);
@@ -20,35 +28,14 @@ function App() {
       });
   }
 
-  let onClose = (id) => {
-    const newCharacters = characters.filter(
-      (character) => character.id !== id
-    )
-    setCharacters(newCharacters);
-  };
-
   return (
     <div className="App" style={{ padding: "25px" }}>
-      {/* <div>
-        <Card
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert('Emulamos que se cierra la card')}
-        />
-      </div> */}
       <Nav onSearch={onSearch} />
       <hr />
       <div>
         <Cards onClose={onClose} characters={characters} />
       </div>
       <hr />
-      {/* <div>
-        <SearchBar
-          onSearch={(characterID) => window.alert(characterID)}
-        />
-      </div> */}
     </div>
   );
 }
