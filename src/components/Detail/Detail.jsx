b2395b62502aa7b701d379d590454b540ecaa048
RodrigoPalomo <repmlm@hotmail.com>
2023-03-08 16:30:44 -0300

    changes

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
+
+// test
+
 const Detail = () => {
   const { detailId } = useParams();
   const [character, setCharacter] = useState({});
