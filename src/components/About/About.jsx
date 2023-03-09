1399c8fa7a8a2de0f879a8b1847e549fc70ceff3
RodrigoPalomo <repmlm@hotmail.com>
2023-03-09 16:32:05 -0300

    About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      About
+      <p>No tengo mucho que poner en el about,</p>
+      <p>No está terminada mi humilde app, pero</p>
+      <p>sin dudas es algo que me quedará pendiente.</p>
     </div>
   )
 }
